902891b464c91cb039d4a1eb5d1bafa1e010a3f7
Asumu Takikawa <asumu@igalia.com>
2024-08-19 12:48:17 -0700

    Fixup: fix path
    Need a short description (OOPS!).
    Need the bug URL (OOPS!).
    
    Reviewed by NOBODY (OOPS!).
    
    Explanation of why this fixes the bug (OOPS!).
    
    * JSTests/modules/import-defer-named.js:

--- JSTests/modules/import-defer-named.js
+++ JSTests/modules/import-defer-named.js
@@ -1,6 +1,6 @@
 import { shouldBe } from "./resources/assert.js"
 
-await import('./import-defer/named.js')
+await import('./import-defer-named/named.js')
     .then($vm.abort, function (error) {
         shouldBe(String(error), `SyntaxError: Unexpected token '{'. Cannot parse the namespace import.`);
     }).catch($vm.abort);
